ac79d10e8fcf4fd8970444633d82881f1549dc97
Ash Blue <ash@ashbluewebdesign.com>
2012-08-03 16:48:56 -0500

    Indexed DB fix.

--- js/interface.js
+++ js/interface.js
@@ -9,18 +9,40 @@
 var db;
 
 (function () {
+    /** @type {object} Database request */
+    _request = null;
+
+    /** @type {object} Cached instance of a successful DB response */
+    _db = null;
+
     db = {
         init: function () {
             // This will improve our code to be more readable and shorter
             window.indexedDB = window.indexedDB || window.webkitIndexedDB || window.mozIndexedDB || window.msIndexedDB;
-            
+
+            this.testDB(window.indexedDB);
+
             // Now we can open our database
-            var request = window.indexedDB.open("MyTestDatabase");
-            console.log(request);
+            _request = window.indexedDB.open("MyTestDatabase");
+
+            // Setup error handeling
+            _request.onerror = function () {
+                throw new Error('db crashed during initialization.');
+            };
+
+            // Setup success handeling
+            _request.onsuccess = function (e) {
+                console.info('DB setup correctly');
+
+                // Store the retrieved database result when its ready
+                _db = _request.response;
+            };
         },
 
-        testDB: function () {
-
+        testDB: function (dbData) {
+            if (dbData === undefined) {
+                throw new Error('No Indexed DB support.');
+            }
         }
     };
 
