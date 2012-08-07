a2b4a2589d3b9908791189ebebaf060dd582de4c
Ash Blue <ash@ashbluewebdesign.com>
2012-08-07 00:29:36 -0500

    Shim for older implementation of indexedDB

--- js/interface.js
+++ js/interface.js
@@ -11,16 +11,16 @@ var db;
 
 (function () {
     /** @type {object} Database request */
-    _request = null;
+    var _request = null;
 
     /** @type {object} Cached instance of a successful DB response */
-    _db = null;
+    var _db = null;
 
     SELF = null;
 
     db = {
         init: function () {
-            if (SELF !== null) {
+            if (typeof SELF !== 'object') {
                 return;
             }
 
@@ -38,7 +38,7 @@ var db;
 
         requestDB: function (callback) {
             // Now we can open our database
-            _request = window.indexedDB.open("MyTestDatabase");
+            _request = window.indexedDB.open("Testas", 2); // (dbName, version)
 
             // Setup error handeling
             _request.onerror = function () {
@@ -47,6 +47,12 @@ var db;
 
             // Setup success handeling
             _request.onsuccess = function (e) {
+                // Shim for browsers using the old implementation
+                if (SELF.hasSetVersion(e)) {
+                    var setVer = e.target.result.setVersion(1);
+                    setVer.onsuccess = SELF.writeData;
+                };
+
                 console.info('DB setup correctly');
 
                 // Store the retrieved database result when its ready
@@ -54,14 +60,46 @@ var db;
 
                 callback();
             };
+
+            // Only place you can actually edit the DB
+            _request.onupgradeneeded = this.writeData;
+        },
+
+        // Detects if the browser supports the old draft of IndexedDB
+        hasSetVersion: function (e) {
+            return e.target.result.__proto__.hasOwnProperty('setVersion');
         },
 
         configureDB: function () {
             console.log(_db);
 
+            // Setup error handeling
             _db.onerror = function (e) {
                 console.error('Database error: ' + e.target.errorCode);
             };
+        },
+
+        /**
+         * Needs extra logic to make sure it isn't trying to overwrite or re-create things
+         */
+        writeData: function (e) {
+            // If the DB is not set, we need to pull from the DB target immediately, doubles as a shim for old browsers
+            var dbWriter = _db || e.target.result;
+
+            var customerData = [
+                { ssn: '444-44-4444', name: 'Bill', age: 35, email: 'mailto:bill@company.com' },
+                { ssn: '555-55-5555', name: 'Donna', age: 32, email: 'mailto:donna@home.org' }
+            ];
+
+            var table = dbWriter.createObjectStore('customers', { keyPath: 'ssn' });
+            table.createIndex('name', 'name', { unique: false });
+            table.createIndex('email', 'email', { unique: true });
+
+            for (var i in customerData) {
+                table.add(customerData[i]);
+            }
+
+            console.info('onupgradeneeded event fired');
         },
 
         testDB: function (dbData) {
@@ -70,6 +108,4 @@ var db;
             }
         }
     };
-
-
 }());
